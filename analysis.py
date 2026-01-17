7add843aa92cf0da59a502da4e111a67ff159846
viktoriia-kozak <viktoriia.kozak7@gmail.com>
2026-01-17 22:20:39 +0100

    add average sales by city

--- analysis.py
+++ analysis.py
@@ -1,2 +1,4 @@
 def total_sales_by_city(df):
     return df.groupby("city")["sales"].sum()
+def average_sales_by_city(df):
+    return df.groupby("city")["sales"].mean()
--- report.py
+++ report.py
@@ -5,3 +5,8 @@ df = load_data()
 result = total_sales_by_city(df)
 print("Загальні продажі по містах:")
 print(result)
+from analysis import average_sales_by_city
+
+avg = average_sales_by_city(df)
+print("\nСередні продажі по містах:")
+print(avg)
